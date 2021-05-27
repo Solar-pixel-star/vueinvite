52e685934368f3293560433bfda2d849715d626b
sidney rosenberg <edwardcansing@gmail.com>
2021-05-27 17:04:34 -0400

    Fancy stuff

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,13 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  h1 {
-    color: white;
-  }
-  a {
-    color: aqua;
-  }
-  body {
-    background-color: black;
-  }
+body {
+  color: black;
 }
+
+
+h1 {
+  color: white;
+}
+
 </style>
